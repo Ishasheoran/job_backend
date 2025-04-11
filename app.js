f9acec2bc0214190a4a706e6c157adb645cd4337
Isha <167119186+Ishasheoran@users.noreply.github.com>
2025-04-11 17:31:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const app = express();
 config({ path: "./config/config.env" });
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "job-frontend-plum.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
